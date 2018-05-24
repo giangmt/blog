1919183c4914ca433f842d74f9d11f306985a5db
Mai Truong Giang <34191661+giangmt@users.noreply.github.com>
2018-05-25 03:13:44 +0700

    Revert "login with github"
    This reverts commit b4c7b1f4c2dafd79ef60337cc543eb2b8400265b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root to: "articles#index"
-
-  get "/auth/:provider/callback", to: "sessions#create"
-  get "auth/failure", to: redirect("/")
-  delete "/signout", to: "sessions#destroy"
   
   resources :articles
 end
